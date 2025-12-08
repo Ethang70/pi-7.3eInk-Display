b17ce2ef1982d48b3873b0024c1b6e02ec66b9cd
Ethan <egilly70@gmail.com>
2025-12-08 23:50:52 +1030

    Enlarged Icon and Text

--- image.py
+++ image.py
@@ -7,7 +7,7 @@ from libs import google_weather as gw
 
 def get_image(epd):
     try:
-        font24 = ImageFont.load_default(48)
+        font = ImageFont.load_default(84)
         
         weather = gw.get_current_weather_display_info()
         
@@ -15,21 +15,22 @@ def get_image(epd):
         
         #logging.info("1.Drawing on the image...")
         Himage = Image.new('RGB', (epd.width, epd.height), epd.WHITE)
-        Himage = Image.open(weather["condition_icon"])
-        Himage = Image.composite(Himage, Image.new('RGB', (epd.width, epd.height), epd.WHITE), Himage)
+        condition = Image.open(weather["condition_icon"])
+        condition = condition.resize((200, 200), Image.NEAREST)
+        Himage.paste(condition, (25, 25), condition)
         draw = ImageDraw.Draw(Himage)
-        draw.text((100, 20), str(weather["temperature"]), font = font24, fill = epd.BLACK)
+        draw.text((235, 64.5), str(weather["temperature"]), font = font, fill = epd.BLACK)
         
         
-        Himage = color_epd_converter.convert(Himage,
-                                      orientation="landscape",
-                                      width=480,
-                                      height=800,
-                                      crop_image=False,
-                                      crop_x1=0,
-                                      crop_y1=0,
-                                      crop_x2=480,
-                                      crop_y2=800)
+        # Himage = color_epd_converter.convert(Himage,
+        #                               orientation="landscape",
+        #                               width=480,
+        #                               height=800,
+        #                               crop_image=False,
+        #                               crop_x1=0,
+        #                               crop_y1=0,
+        #                               crop_x2=480,
+        #                               crop_y2=800)
         draw = ImageDraw.Draw(Himage)
         
 
